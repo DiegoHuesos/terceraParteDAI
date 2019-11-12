b9a3d1e2ac6fbe51d6f7396ff1d8f31c8d021c1d
dai <dai@itam.mx>
2019-11-12 12:38:33 -0600

    Inicial

---
+++ pruebaDll3/pruebaDll3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaDll3
+{
+    public class Class1
+    {
+        private int num1 = 5;
+        private int num2 = 3;
+
+        public void setNum1(int n)
+        {
+            this.num1 = n;
+        }
+
+        public void setNum2(int n)
+        {
+            this.num2 = n;
+        }
+
+        public int multiplica()
+        {
+            return num1 * num2;
+        }
+    }
+}
